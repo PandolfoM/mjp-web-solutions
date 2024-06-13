9544862e192aba0b09466cd892134a7a54a172d0
Matthew Pandolfo <matt@pandolfo.com>
2024-06-13 17:49:35 -0400

    test route

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -9,21 +9,25 @@ export async function POST(req: Request) {
   const { email, firstName, lastName, subject, msg } = await req.json();
 
   try {
-    const { data, error } = await resend.emails.send({
-      from: "MJP Web Solutions <mjp@mattpandolfo.com>",
-      to: email,
-      subject: `Website Inquiry (${subject})`,
-      react: InquiryTemplate({ firstName, lastName, email, msg, subject }) as React.ReactElement,
+    return NextResponse.json({
+      success: true,
+      message: "successful post to /send",
     });
+    // const { data, error } = await resend.emails.send({
+    //   from: "MJP Web Solutions <mjp@mattpandolfo.com>",
+    //   to: email,
+    //   subject: `Website Inquiry (${subject})`,
+    //   react: InquiryTemplate({ firstName, lastName, email, msg, subject }) as React.ReactElement,
+    // });
 
-    if (error) {
-      console.error('Resend API error:', error);
-      return NextResponse.json({ error }, { status: 500 });
-    }
+    // if (error) {
+    //   console.error('Resend API error:', error);
+    //   return NextResponse.json({ error }, { status: 500 });
+    // }
 
-    return NextResponse.json(data);
+    // return NextResponse.json(data);
   } catch (error) {
-    console.error('Unexpected error:', error);
+    console.error("Unexpected error:", error);
     return NextResponse.json({ error }, { status: 500 });
   }
 }
