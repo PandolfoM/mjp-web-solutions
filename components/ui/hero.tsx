a176d168c088bfefa43600335c7b88d93089af4a
Matthew Pandolfo <matt@pandolfo.com>
2024-09-11 08:52:11 -0400

    update header in hero

--- components/ui/hero.tsx
+++ components/ui/hero.tsx
@@ -5,7 +5,7 @@ import Button from "./button";
 function Hero() {
   return (
     <section className="text-center px-7">
-      <h1 className="font-bold text-lg">Lorem ipsum dolor sit amet.</h1>
+      <h1 className="font-bold text-lg">Creating your Digital Presence.</h1>
       <p className={`${jetBrains.className} text-sm opacity-75 pb-[25px]`}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel
         fringilla orci. Curabitur auctor orci et quam porta lacinia. Nam
