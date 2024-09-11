5a1be5a250a9bbcb6832e6108d21859d80c208ef
Matthew Pandolfo <matt@pandolfo.com>
2024-09-11 16:30:00 -0400

    add hero text and favicon

--- components/ui/hero.tsx
+++ components/ui/hero.tsx
@@ -1,17 +1,19 @@
 import { jetBrains } from "@/app/fonts";
 import React from "react";
 import Button from "./button";
+import Link from "next/link";
 
 function Hero() {
   return (
     <section className="text-center px-7">
       <h1 className="font-bold text-lg">Creating your Digital Presence.</h1>
       <p className={`${jetBrains.className} text-sm opacity-75 pb-[25px]`}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel
-        fringilla orci. Curabitur auctor orci et quam porta lacinia. Nam
-        fermentum laoreet ligula vitae hendrerit.
+        Unlock the potential of your brand with a strong digital presence that
+        speaks to your audience and drives engagement.
       </p>
-      <Button className="w-[230px] h-[35px]" />
+      <Link href="/contact">
+        <Button className="w-[230px] h-[35px]" />
+      </Link>
     </section>
   );
 }
