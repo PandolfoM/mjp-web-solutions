706cd2b41127baa03c76efa992ac8ef2dd8c81ea
Matthew Pandolfo <matt@pandolfo.com>
2024-09-11 21:30:07 -0400

    footer for desktop

--- components/navigation/footer.tsx
+++ components/navigation/footer.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
 
 function Footer() {
   return (
-    <footer className='bg-[#7B61FF]/[2%] h-9 mt-[100px] flex items-center px-7 flex-shrink-0'>
-      <p className='text-xxs opacity-50'>&copy; 2024 MJP Web Solutions.</p>
+    <footer className="bg-[#7B61FF]/[2%] h-9 mt-[100px] flex items-center px-7 flex-shrink-0">
+      <p className="text-xxs opacity-50 lg:text-xs">
+        &copy; 2024 MJP Web Solutions.
+      </p>
     </footer>
   );
 }
