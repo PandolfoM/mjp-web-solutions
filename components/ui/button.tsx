c32d598aa1b868a5de0f25c6556996f9d48487d9
Matthew Pandolfo <matt@pandolfo.com>
2024-09-13 19:00:58 -0400

    use cn util on button styles

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,15 +1,19 @@
+import { cn } from "@/lib/utils";
 import React from "react";
 
 type Props = {
   text?: string;
   className?: string;
   disabled?: boolean;
-  type?: "button" | "submit" | "reset" | undefined
+  type?: "button" | "submit" | "reset" | undefined;
 };
 
 function Button({ text, className, type, disabled }: Props) {
   return (
-    <button type={type} className={`p-1 px-3 font-bold custom-border ${className}`} disabled={disabled} >
+    <button
+      type={type}
+      className={cn("p-1 px-3 font-bold custom-border", className)}
+      disabled={disabled}>
       {text ? text : "Get Started"}
     </button>
   );
