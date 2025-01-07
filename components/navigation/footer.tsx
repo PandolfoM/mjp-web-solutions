984fe61a4c8325f8ffe013428e3031e0723cba9a
Matthew Pandolfo <matt@pandolfo.com>
2025-01-06 20:07:54 -0500

    Update footer.tsx

--- components/navigation/footer.tsx
+++ components/navigation/footer.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="bg-[#7B61FF]/[2%] h-9 mt-[100px] flex items-center px-7 flex-shrink-0">
       <p className="text-xxs opacity-50 lg:text-xs">
-        &copy; 2024 MJP Web Solutions.
+        &copy; {currentYear} MJP Web Solutions.
       </p>
     </footer>
   );
